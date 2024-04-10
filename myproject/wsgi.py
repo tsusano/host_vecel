832928944ef3349d36c527f4f20b020764e4b0ec
Chandrabindu <96874994+tsusano@users.noreply.github.com>
2024-04-11 04:02:33 +0530

    3rd commit

--- myproject/wsgi.py
+++ myproject/wsgi.py
@@ -8,10 +8,13 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
+
+if os.environ.get("env") == "production":
+    sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
 application = get_wsgi_application()
 app = application
